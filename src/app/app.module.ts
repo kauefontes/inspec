3cfe924693134e66d80e8490536b811cf0828fd4
Kaue Pereira <kauefontes@outlook.com>
2018-02-17 19:24:13 -0400

    created companies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { CompaniesComponent } from './companies/companies.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CompaniesComponent
   ],
   imports: [
     BrowserModule,
